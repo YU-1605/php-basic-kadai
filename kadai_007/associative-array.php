7e6cb7f81e4b833d7ee23be796b0693727151e66
YU-1605 <tagu1517@gmail.com>
2024-10-18 11:42:58 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head></head>
+  <body>
+    <p>
+      <?php
+        $vegetable = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        
+        print_r($vegetable);
+      ?>
+    </p>
+  </body>
+</html>
